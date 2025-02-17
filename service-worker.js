eb3c768ff12b04a565e23c87bac967f0d20282d5
usul-ad-deen <abdul-rahman.itani@hotmail.de>
2025-02-17 19:45:34 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,26 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = "usul-ad-deen-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/script.js",
+    "/manifest.json",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open("usul-ad-deen-cache").then((cache) => {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/styles.css",
-                "/script.js",
-                "/icons/icon-192x192.png",
-                "/manifest.json"
-            ]);
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log("Caching aller Ressourcen...");
+            return cache.addAll(urlsToCache);
         })
     );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", function (event) {
     event.respondWith(
-        caches.match(event.request).then((response) => {
+        caches.match(event.request).then(function (response) {
             return response || fetch(event.request);
         })
     );
