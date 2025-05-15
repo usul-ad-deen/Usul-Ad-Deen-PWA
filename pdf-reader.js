a4cc6c997a3603d8456f5bb882e6af1eb7d54d15
usul-ad-deen <abdul-rahman.itani@hotmail.de>
2025-05-15 13:49:52 +0200

    Update pdf-reader.js

--- pdf-reader.js
+++ pdf-reader.js
@@ -153,7 +153,7 @@ function speichereGelesenesBuch() {
 window.fortsetzenLetztesBuch = () => {
   const letzteDatei = localStorage.getItem("zuletzt-gelesen");
   if (letzteDatei) {
-    window.location.href = `pdf-viewer.html?file=${encodeURIComponent(letzteDatei)}`;
+    window.location.href = `pdf-reader.html?file=${encodeURIComponent(letzteDatei)}`;
   } else {
     alert("⚠️ Kein zuletzt gelesenes Buch gefunden.");
   }
