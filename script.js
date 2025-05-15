c0301a1bcc1a92024c13681db768ea2ebe9e3fd6
usul-ad-deen <abdul-rahman.itani@hotmail.de>
2025-05-15 13:50:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -486,7 +486,7 @@ ladeFeiertagsCountdowns("Berlin");
     window.fortsetzenLetztesBuch = () => {
   const letzteDatei = localStorage.getItem("zuletzt-gelesen");
   if (letzteDatei) {
-    window.location.href = `pdf-viewer.html?file=${encodeURIComponent(letzteDatei)}`;
+    window.location.href = `pdf-reader.html?file=${encodeURIComponent(letzteDatei)}`;
   } else {
     alert("⚠️ Kein zuletzt gelesenes Buch gespeichert.");
   }
